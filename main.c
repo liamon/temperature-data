99f1755002fb7e46791671dc1c061ebe7904adfc
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-09-03 16:37:15 +0100

    Fixed comment explaining argc % 2 == 0

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "tempdata.h"
 
 int main(int argc, char *argv[]) {
-	if (argc % 2 == 0) { // Checks
+	if (argc % 2 == 0) { // Checks if argc is even (it should be odd, towns and temperatures + executable name)
 		puts("ERROR: Data entered incorrectly -- ending program");
 		return 1;
 	}
